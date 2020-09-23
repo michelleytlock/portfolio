dcf61415e888620c9fe69c2dd6a651d201116633
Michelle Lock <michelleloc@gmail.com>
2020-09-23 22:13:39 +0800

    Fix color of link in alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,22 @@
-import React from 'react';
-import '../styles/Alert.css'
+import React from "react"
+import "../styles/Alert.css"
 
 function Alert() {
-   return (
-      <div className="alert">
-         <h2>🚧 Sorry! Parts of my site are still under construction. 🚧</h2>
-        <h3>If you have any questions, please view my temporary portfolio <a href="https://www.notion.so/Meet-Michelle-72456c13129b486f9cd9160dfc9ed69f" target="_blank">here</a> and contact me directly at michelleytlock@gmail.com!</h3>
-      </div>
-   )
+  return (
+    <div className="alert">
+      <h2>🚧 Sorry! Parts of my site are still under construction. 🚧</h2>
+      <h3>
+        If you have any questions, please view my temporary portfolio{" "}
+        <a className="alert__link"
+          href="https://www.notion.so/Meet-Michelle-72456c13129b486f9cd9160dfc9ed69f"
+          target="_blank"
+        >
+          here
+        </a>{" "}
+        and contact me directly at michelleytlock@gmail.com!
+      </h3>
+    </div>
+  )
 }
 
 export default Alert
