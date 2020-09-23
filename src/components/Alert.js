c0af79ba12312c05eb95efce1e160a5cf1607cd0
Michelle Lock <michelleloc@gmail.com>
2020-09-23 21:36:54 +0800

    Update favicon

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -5,7 +5,7 @@ function Alert() {
    return (
       <div className="alert">
          <h2>🚧 Sorry! Parts of my site are still under construction. 🚧</h2>
-        <h3>If you have any questions please contact me directly!</h3>
+        <h3>If you have any questions please contact me directly at michelleytlock@gmail.com!</h3>
       </div>
    )
 }
