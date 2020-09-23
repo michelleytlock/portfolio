8d0cad39762e6697214a84e24ebac1b9a5edbcf1
Michelle Lock <michelleloc@gmail.com>
2020-09-23 21:46:17 +0800

    Edit alert message

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -5,7 +5,7 @@ function Alert() {
    return (
       <div className="alert">
          <h2>🚧 Sorry! Parts of my site are still under construction. 🚧</h2>
-        <h3>If you have any questions please contact me directly at michelleytlock@gmail.com!</h3>
+        <h3>If you have any questions, please view my temporary portfolio <a href="https://www.notion.so/Meet-Michelle-72456c13129b486f9cd9160dfc9ed69f" target="_blank">here</a> and contact me directly at michelleytlock@gmail.com!</h3>
       </div>
    )
 }
